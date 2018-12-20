1b802ba7c9259f16e7cb78d145502823eddf1b33
regnex <sunumacbright@gmail.com>
2018-12-20 11:35:31 +0000

    added jokeLibrary and jokeProvider

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,10 +1,8 @@
 package com.udacity.gradle.builditbigger.backend;
 
 import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -19,12 +17,19 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
+ /*   @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
         return response;
-    }
+    }*/
+
+//    @ApiMethod(name = "getJoke")
+//    public MyBean getJoke(){
+//        JokeProvider jokeProvider = new J
+//        return response;
+//    }
+
 
 }
