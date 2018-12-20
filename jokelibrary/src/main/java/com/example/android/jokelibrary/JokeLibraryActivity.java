26bb693deca7be6f346961e5991065f7983656b2
regnex <sunumacbright@gmail.com>
2018-12-20 11:46:03 +0000

    get jokes from api

---
+++ jokelibrary/src/main/java/com/example/android/jokelibrary/JokeLibraryActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.jokelibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class JokeLibraryActivity extends AppCompatActivity {
+
+	public static final String EXTRA_JOKE_TEXT = "joke-text";
+	private TextView mJokeDisplayTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_joke_library);
+
+		if (savedInstanceState == null) {
+			Intent intentFromCallingActivity = getIntent();
+			if (intentFromCallingActivity != null && intentFromCallingActivity.hasExtra(EXTRA_JOKE_TEXT)) {
+				String jokeTextFromIntent = intentFromCallingActivity.getStringExtra(EXTRA_JOKE_TEXT);
+
+				mJokeDisplayTextView = findViewById(R.id.tv_joke_display);
+
+				mJokeDisplayTextView.setText(jokeTextFromIntent);
+
+			} else {
+				Toast.makeText(this, "Error: No joke text found!", Toast.LENGTH_LONG).show();
+				finish();
+			}
+		}
+	}
+}
