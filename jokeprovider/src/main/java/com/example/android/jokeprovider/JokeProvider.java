551a906a02792a36a7fa3a750b9f69033bb98f8e
regnex <sunumacbright@gmail.com>
2018-12-21 13:21:04 +0000

    added picture

--- jokeprovider/src/main/java/com/example/android/jokeprovider/JokeProvider.java
+++ jokeprovider/src/main/java/com/example/android/jokeprovider/JokeProvider.java
@@ -1,20 +1,16 @@
 package com.example.android.jokeprovider;
 
-import java.util.Random;
 
 public class JokeProvider {
-	private Random random = new Random();
-	private String[] awesomeJokes =
-			{"How can Santa deliver presents during a thunderstorm? \n His sleigh is flown by raindeer",
-					"What is a mountain's favorite type of candy? \n Snow caps",
-					"What do you call dangerous precipitation? \n A rain of terror",
-					"Where do lightning bolts go on dates? \n To Cloud 9"};
+	public static String getJoke() {
+		String[] objects = {"How can Santa deliver presents during a thunderstorm? \n\n His sleigh is flown by raindeer",
+				"What is a mountain's favorite type of candy? \n\n Snow caps",
+				"What do you call dangerous precipitation? \n\n A rain of terror",
+				"Where do lightning bolts go on dates? \n\n To Cloud 9"};
 
-	//https://stackoverflow.com/questions/5887709/getting-random-numbers-in-java
-	public String getJoke() {
-		if (random == null) {
-			random = new Random();
-		}
-		return awesomeJokes[random.nextInt(3)];
+		int length = objects.length;
+		int rand = (int) (Math.random() * length);
+
+		return objects[rand];
 	}
 }
